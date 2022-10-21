f7615d58bc294906c9686402a5c1d1474dbaaad1
Alyssa Barbieri <19173672+alyssa-barbieri@users.noreply.github.com>
2022-10-21 13:08:25 +1300

    update route and server files along with the plants view to hold handlebars info

--- routes.js
+++ routes.js
@@ -1,15 +1,31 @@
 const fs = require('fs').promises
 const express = require('express')
+const server = require('./server')
 const router = express.Router()
 
 //routes
 
-router.get('/plants/:id', (req, res) => {
-  const id = Number(req.params.id)
+router.get('/', (req, res) => {
   fs.readFile('./data.json', 'utf-8')
     .then((data) => {
-      const parsedData = JSON.parse(data)
-      const singlePlant = parsedData.plant.find((plant) => plant.id === id)
+      return res.render('home', JSON.parse(data))
+    })
+    .catch((err) => {
+      return res.status(500).send(err.message)
+    })
+})
+
+router.get('/plants/', (req, res) => {
+  res.render('plants')
+})
+
+router.get('/plants/:id', (req, res) => {
+  const plant = Number(req.params.id)
+  fs.readFile('./data.json', 'utf-8')
+    .then((plants) => {
+      const parsedData = JSON.parse(plants)
+      const singlePlant = parsedData.plants.find((plant) => plant.id === plant)
+      console.log(plants.id)
       return res.render('plants', singlePlant)
     })
     .catch((err) => {
@@ -17,31 +33,30 @@ router.get('/plants/:id', (req, res) => {
     })
 })
 
-router.post('/:id/edit', (req, res) => {
-  const id = Number(req.params.id)
-  // const newBreed = req.body.breed
-  // const newName = req.body.name
-  // const newOwner = req.body.owner
+// update this for comments page
+// router.post('/:id/comment', (req, res) => {
+//   const id = Number(req.params.id)
+//   const comments = req.body.comments
 
-  fs.readFile('./data.json', 'utf-8')
-    // .then(animals => {
-    //   const animalsCopy = JSON.parse(animals)
+//   fs.readFile('./data.json', 'utf-8')
+//     .then((plants) => {
+//       const plantsCopy = JSON.parse(plants)
 
-    //   const onePup = animalsCopy.puppies.find(animal => {
-    //   return animal.id === id
-    // })
-    //   onePup.owner = newOwner
-    //   onePup.name = newName
-    //   onePup.breed = newBreed
-    //   return animalsCopy
-    // })
-    // .then(animals => {
-    // return fs.writeFile('./data.json', JSON.stringify(animals, null, 4))
-    // })
-    // .then(() => { return res.redirect('/puppies/' + id) })
-    .catch((err) => {
-      return res.status(400).send(err.message)
-    })
-})
+//       const specificPlant = plantsCopy.plants.find((plants) => {
+//         return plants.id === id
+//       })
+//       specificPlants.comments = newComments
+//       return newComments
+//     })
+//     .then((comments) => {
+//       return fs.writeFile('./comments.json', JSON.stringify(plants, null, 4))
+//     })
+//     .then(() => {
+//       return res.redirect('/plants/' + id)
+//     })
+//     .catch((err) => {
+//       return res.status(500).send(err.message)
+//     })
+// })
 
 module.exports = router
